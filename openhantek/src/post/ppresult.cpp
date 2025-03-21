809c044fb1566625edb623c0c639827a4a896ee2
Dale Schultz <dschultzca@yahoo.ca>
2025-03-21 13:33:08 -0400

    Fix Ch2 fast rate sample count at top of scope display

--- openhantek/src/post/ppresult.cpp
+++ openhantek/src/post/ppresult.cpp
@@ -14,6 +14,16 @@ const DataChannel *PPresult::data(ChannelID channel) const {
 
 DataChannel *PPresult::modifyData(ChannelID channel) { return &this->analyzedData[(size_t)channel]; }
 
-unsigned int PPresult::sampleCount() const { return (unsigned)analyzedData[0].voltage.sample.size(); }
+unsigned int PPresult::sampleCount() const {
+    // Figure out which channel has all the fast rate samples
+    if ((unsigned)analyzedData.size() > 1) {
+        if ((unsigned)analyzedData[0].voltage.sample.size() == 0 &&
+            (unsigned)analyzedData[1].voltage.sample.size() > 10240) {
+            // channel 2 has all the samples
+            return (unsigned)analyzedData[1].voltage.sample.size();
+        }
+    }
+    return (unsigned)analyzedData[0].voltage.sample.size();
+}
 
 unsigned int PPresult::channelCount() const { return (unsigned)analyzedData.size(); }
